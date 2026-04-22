4aae7b433582f199e6be53718c7b47e13ead77a5
hahaclassic <gavrilyukvlad221@gmail.com>
2026-04-23 00:19:57 +0300

    be-43-fix/permissions: fixed permission middleware

--- internal/providers/http/middleware/permissions.go
+++ internal/providers/http/middleware/permissions.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tiptop-co/backend/internal/model"
 	"github.com/tiptop-co/backend/internal/model/authz"
@@ -11,12 +13,12 @@ func RequirePermission(p authz.Permission) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		claims := ctxclaims.GetClaims(c)
 		if claims == nil {
-			_ = c.Error(model.ErrUnauthorized)
+			_ = c.AbortWithError(http.StatusUnauthorized, model.ErrUnauthorized)
 			return
 		}
 
 		if !authz.HasPermission(claims.UserRole, p) {
-			_ = c.Error(model.ErrForbidden)
+			_ = c.AbortWithError(http.StatusForbidden, model.ErrForbidden)
 		}
 
 		c.Next()
