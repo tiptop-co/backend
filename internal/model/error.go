c5a9fb5d07364902a77b616c0066713eae8dca2e
hahaclassic <gavrilyukvlad221@gmail.com>
2026-04-26 23:01:26 +0300

    be-21-feature/tables: updated general errors

--- internal/model/error.go
+++ internal/model/error.go
@@ -3,8 +3,9 @@ package model
 import "errors"
 
 var (
-	ErrNotFound     = errors.New("not found")
-	ErrValidation   = errors.New("validation error")
-	ErrUnauthorized = errors.New("unauthorized")
-	ErrForbidden    = errors.New("forbidden")
+	ErrNotFound      = errors.New("not found")
+	ErrValidation    = errors.New("validation error")
+	ErrUnauthorized  = errors.New("unauthorized")
+	ErrForbidden     = errors.New("forbidden")
+	ErrAlreadyExists = errors.New("already exists")
 )
